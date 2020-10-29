d231ad84cf00a97c133080c53bca277b6a3c92e9
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2020-10-29 08:47:32 +0100

    added basic string application

--- app/controllers/Connect4Controller.scala
+++ app/controllers/Connect4Controller.scala
@@ -1,7 +1,15 @@
 package controllers
 
+import Connect4._
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 class Connect4Controller(cc:ControllerComponents) extends AbstractController(cc){
+  val gameController = Connect4.controller
+
+  def connect4Test = gameController.grid.toString
+
+  def connect4 = Action{
+    Ok(connect4Test)
+  }
 
 }
