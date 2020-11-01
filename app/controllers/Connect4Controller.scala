c740f69cf3d0c4b2cd4b55b8331b3bc70b33f71a
OenderTuetuenci <oender.tuetuenci@gmx.de>
2020-11-01 12:00:41 +0100

    worked a bit on the Wui

--- app/controllers/Connect4Controller.scala
+++ app/controllers/Connect4Controller.scala
@@ -4,19 +4,20 @@ import Connect4._
 import controller.controllerComponent.ControllerInterface
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import model.gridComponent.GridInterface
 
 
 class Connect4Controller @Inject() (cc:ControllerComponents) extends AbstractController(cc){
   val gameController:ControllerInterface = Connect4.controller
-
-  def gridText: String = gameController.grid.toString
+  def grid:GridInterface = gameController.grid
+  def players:List[Int] = gameController.players
 
   def connect4: Action[AnyContent] = Action{
-    Ok(gridText)
+    Ok(views.html.connect4(grid,players.head.toString))
   }
   def put(column: Int): Action[AnyContent] = Action{
     gameController.move(column)
-    Ok(gridText)
+    Ok(views.html.connect4(grid,gameController.players.head.toString))
   }
   def about(): Action[AnyContent] = Action {
     Ok(views.html.index())
