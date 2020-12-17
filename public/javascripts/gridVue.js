31e626087efdb2b48d6d7b67889cb6c77a7a22b2
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2020-12-17 12:17:02 +0100

    changed greeting in vue to prompt the player to enter the name and only show name after enter

--- public/javascripts/gridVue.js
+++ public/javascripts/gridVue.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
+    // noinspection JSUnusedLocalSymbols
     let app = new Vue({
             el: '#app',
             data: {
-                title: '',
                 name: '',
+                welcome: 'Wilkommen, bitte gib deinen Namen ein: '
             },
             methods:{
                 submit:function(){
                     $("#eingabe").hide();
+                    this.welcome= 'Wilkommen ' + this.name;
                 }
             }
+
         }
     )
 })
